0f62c4e5ff419ead662df25973c43e507878dd34
Florian Sihler <florian.sihler@uni-ulm.de>
2022-05-01 19:42:10 +0200

    p for tut1

---
+++ 1-Tutorium/BoolescheAusdruecke.java
@@ -0,0 +1,8 @@
+class BoolescheAusdruecke {
+   public static void main(String[] args) {
+      float temperatur = 18.0f;
+      short alter = 15;
+      System.out.println(temperatur <= 22.5f);
+      System.out.println(!(13 <= alter && alter <= 18));
+   }
+}
