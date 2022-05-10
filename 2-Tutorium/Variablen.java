22438ef8d41d31a22c3cbd0cceac753d1b2f6a7b
Florian Sihler <florian.sihler@uni-ulm.de>
2022-05-11 00:14:13 +0200

    2.4

---
+++ 2-Tutorium/Variablen.java
@@ -0,0 +1,9 @@
+class Variablen {
+   public static void main(String[] args) {
+      int zahl1, zahl2, ergebnis; // Deklaration
+      zahl1 = 10;                 // Initialisierung
+      zahl2 = 10;                 // Initialisierung
+      ergebnis = zahl1 + zahl2;   // Initialisierung
+      System.out.println(ergebnis);
+   }
+}
