ebc71604e7f6e126fb2fdb0ba0d0e21b8e5f8bad
Florian Sihler <florian.sihler@uni-ulm.de>
2022-05-25 00:18:44 +0200

    4.2 and animations

--- 4-Tutorium/Matrix.java
+++ 4-Tutorium/Matrix.java
@@ -16,22 +16,21 @@ public class Matrix {
          System.out.println("Eingabe ungültig!");
          return -1.0;
       }
-      double max_zsm = Double.MIN_VALUE;
+      double max = Double.MIN_VALUE;
       double norm;
       for (double[] array : mat) {
          norm = berechneBetragssumme(array);
-         if (norm == -1.0)
+         if (norm == -1.0) // Fehler in Unteralgorithmus?
             return -1.0;
-         if (norm > max_zsm)
-            max_zsm = norm;
+         if (norm > max)
+            max = norm;
       }
-      return max_zsm;
+      return max;
    }
 
    public static void main(String[] args) {
       double[][] matrix = { { 1.5, -2.8, 1.8 }, { -5.8, 1.8, 6.1 }, { -2.6, -4.5, 1.24 } };
       double zsm = berechneZeilensummennorm(matrix);
       System.out.printf("Die Zeilensummennorm beträgt %.2f\n", zsm);
-
    }
 }
