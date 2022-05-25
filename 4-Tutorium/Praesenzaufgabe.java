4bf6db5641fa54e1b14ae42648915dfd0930a565
Florian Sihler <florian.sihler@uni-ulm.de>
2022-05-25 16:05:55 +0200

    new p-task

--- 4-Tutorium/Praesenzaufgabe.java
+++ 4-Tutorium/Praesenzaufgabe.java
@@ -1,35 +1,49 @@
 public class Praesenzaufgabe {
-   public static void flipInPlace(char[] flipBuchstaben) {
-       final int shift = 'a' - 'A'; // der shift ändert sich ja nicht :D
-       for(int i = 0; i < flipBuchstaben.length; i++) {
-           if ('A' <= flipBuchstaben[i] && flipBuchstaben[i] <= 'Z') {
-               flipBuchstaben[i] += shift;
-           } else if ('a' <= flipBuchstaben[i] && flipBuchstaben[i] <= 'z') {
-               flipBuchstaben[i] -= shift;
-           }
-       }
-   }
+    public static void flipInPlace(char[] flipBuchstaben) {
+        final int shift = 'a' - 'A'; // der shift ändert sich ja nicht :D
+        // Damit kann man ihn auch als Konstante umsetzen
+        for (int i = 0; i < flipBuchstaben.length; i++) {
+            if ('A' <= flipBuchstaben[i] && flipBuchstaben[i] <= 'Z') {
+                flipBuchstaben[i] += shift;
+            } else if ('a' <= flipBuchstaben[i] && flipBuchstaben[i] <= 'z') {
+                flipBuchstaben[i] -= shift;
+            }
+        }
+    }
 
-   public static char[] flipInCopy(char[] flipBuchstaben) {
+    public static char[] flipInCopy(char[] flipBuchstaben) {
         char[] arrayKopie = new char[flipBuchstaben.length];
-        for(int i = 0; i < arrayKopie.length; i++)
+        for (int i = 0; i < arrayKopie.length; i++)
             arrayKopie[i] = flipBuchstaben[i];
+        // Auch: System.arraycopy oder Arrays.copyOf
 
-       flipInCopy(arrayKopie);
-       return arrayKopie;
-   }
+        flipInCopy(arrayKopie);
+        return arrayKopie;
+    }
 
-   public static void main(String[] args) {
-       String s = "DieSeR SatZ HaT EinE merKwüRdige Groß- und KleinschreibunG.";
-       System.out.println(s);
+    public static char[] flipInCopyDirect(char[] flipBuchstaben) {
+        char[] arrayKopie = new char[flipBuchstaben.length];
+        for (int i = 0; i < flipBuchstaben.length; i++) {
+            if ('A' <= flipBuchstaben[i] && flipBuchstaben[i] <= 'Z') {
+                arrayKopie[i] = flipBuchstaben[i] + shift;
+            } else if ('a' <= flipBuchstaben[i] && flipBuchstaben[i] <= 'z') {
+                arrayKopie[i] = flipBuchstaben[i] - shift;
+            }
+        }
+        return arrayKopie;
+    }
 
-       char[] flipBuchstaben = s.toCharArray();
+    public static void main(String[] args) {
+        String s = "DieSeR SatZ HaT EinE merKwüRdige Groß- und KleinschreibunG.";
+        System.out.println(s);
 
-       String inCopy = String.valueOf(flipInCopy(flipBuchstaben));
-       System.out.println(inCopy);
+        char[] flipBuchstaben = s.toCharArray();
 
-       flipInPlace(flipBuchstaben);
-       String inPlace = String.valueOf(flipBuchstaben);
-       System.out.println(inPlace);
-   }
+        String inCopy = String.valueOf(flipInCopy(flipBuchstaben));
+        System.out.println(inCopy);
+
+        flipInPlace(flipBuchstaben);
+        String inPlace = String.valueOf(flipBuchstaben);
+        System.out.println(inPlace);
+    }
 }
